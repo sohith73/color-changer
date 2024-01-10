7285d2bcebb0a4bf5c44ab9b0dcbe8cd925ec77e
Sohith Bandreddi <98731252+sohith73@users.noreply.github.com>
2024-01-10 11:52:36 +0530

    Add files via upload
    Testing hosting

---
+++ Index.js
@@ -0,0 +1,19 @@
+let button = document.querySelectorAll('.button');
+let body  = document.querySelector('body');
+
+button.forEach(function (button){
+     button.addEventListener('click', function(e){
+          if (e.target.id === 'grey') {
+               body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'white') {
+               body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'blue') {
+               body.style.backgroundColor = e.target.id;
+          }
+          if (e.target.id === 'yellow') {
+               body.style.backgroundColor = e.target.id;
+          }
+     })
+})
